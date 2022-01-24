9122d26524c4400386251fbbdc1f1ce452db2075
ברדשסקי יבגני <evgenyber@technion.ac.il>
2022-01-24 11:40:52 +0200

    starting to fiddle

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask, render_template, redirect,url_for
 import flask_login
 import flask_sqlalchemy
@@ -8,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def home ():
     return render_template('index.html')
+
+
+if "__main__" == __name__ :
+    app.run(debug=True)
