b135c7d471ddd027a0422447a96086a8dbb80882
ברדשסקי יבגני <evgenyber@technion.ac.il>
2022-01-24 09:47:42 +0200

    first try

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect,url_for
+import flask_login
+import flask_sqlalchemy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home ():
+    return render_template('index.html')
